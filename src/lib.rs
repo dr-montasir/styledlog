4ee16c1d8227b66d503288b72ebe32165ece0692
dr-montasir <montasir1400@gmail.com>
2025-01-29 00:31:03 +0300

    v0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-#![doc(html_logo_url = "https://github.com/dr-montasir/styledlog/raw/HEAD/logo.svg")]
-#![doc = r"<div align='center'><img src='https://github.com/dr-montasir/styledlog/raw/HEAD/logo.svg' alt='logo' width='200' height='200' /></div><br>"]
+#![doc(html_logo_url = "https://raw.githubusercontent.com/dr-montasir/styledlog/main/logo.svg")]
+#![doc = r"<div align='center'><img src='https://raw.githubusercontent.com/dr-montasir/styledlog/main/logo.svg' alt='logo' width='200' height='200' /></div><br>"]
 #![doc = r"<div align='center'>Styledlog: A Rust crate combining versatile logging features with styled output.<br><br>
-<img src='https://github.com/dr-montasir/styledlog/raw/HEAD/styledlog.gif' alt='StyledLog' width='100%' /></div>"]
+<img src='https://raw.githubusercontent.com/dr-montasir/styledlog/main/styledlog.gif' alt='StyledLog' width='100%' /></div>"]
 
 pub mod colorful;
 pub mod formatter;
